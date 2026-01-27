1bcd417dd4f11df873c29bdcaa50c398b111e28e
saranya-3012 <saranyar091103@gmail.com>
2026-01-27 22:36:51 +0530

    Add files via upload

---
+++ Console App.sql
@@ -0,0 +1,12 @@
+use bangtandb;
+
+create table Users(
+     Name varchar(20),
+     Email varchar(30),
+     Phone varchar(20),
+     Password varchar(300)
+     );
+     
+select * from Users;
+
+drop table Users;
---
+++ Usertest.java
@@ -0,0 +1,71 @@
+package pass;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Usertest {
+    public static void main(String[] args) {
+    	try {
+    	
+        UserDAO userDAO = new UserDAO();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== User Menu ===");
+        System.out.println("1. Register");
+        System.out.println("2. Login");
+        System.out.println("3. View All Users");
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
+        sc.nextLine(); 
+
+        switch (choice) {
+            case 1:
+                System.out.print("Enter name: ");
+                String name = sc.nextLine();
+                System.out.print("Enter email: ");
+                String email = sc.nextLine();
+                System.out.print("Enter password: ");
+                String password = sc.nextLine();
+                System.out.print("Enter phone: ");
+                String phone = sc.nextLine();
+
+                String HashedPassword = Passwordvalid.hashPassword(password);
+                Users newUser = new Users(name, email, phone, HashedPassword);
+
+                boolean registered = userDAO.registeruser(newUser);
+                System.out.println(registered ? "Registered successfully!" : "Registration failed!");
+                break;
+
+            case 2:
+                System.out.print("Enter email: ");
+                String e = sc.nextLine();
+                System.out.print("Enter password: ");
+                String p = sc.nextLine();                
+                
+                String HashedPass = Passwordvalid.hashPassword(p);
+                Users user = userDAO.loginUser(e, HashedPass);
+                if (user != null)
+                	System.out.println("✅ Login success! Welcome " + user.getName());
+                else
+                    System.out.println("Invalid email or password!");
+                break;
+                
+            case 3:
+            	System.out.println(" All Users");
+            	List<Users> users = userDAO.getAllUsers();
+                for (Users u : users) {
+                    System.out.println(u.getName() + " | " + u.getEmail() + " | " + u.getPhone() + " | " + u.getPassword());
+                }
+                break;
+
+            default:
+                System.out.println("Invalid choice!");
+        }
+
+        sc.close();
+    	}
+    	catch (Exception e) {
+    		e.getMessage();
+    	}       
+    }
+}
